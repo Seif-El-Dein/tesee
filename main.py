697c265e735af9f17b1e2492caf68df46652ab04
Seif El-Dein <Seif_EL-Dein@outlook.com>
2024-02-26 22:01:28 +0200

    V1.39

---
+++ main.py
@@ -0,0 +1,132 @@
+from Alef_Bot import *
+import _thread
+from utime import sleep_ms, sleep
+from Cards_map import *
+from MQTTHandler import MQTTCommandHandler
+import Internal_LED
+
+
+if __name__ == "__main__":
+    Internal_LED.ON()
+    motor_driver_pins = [7, 6, 26, 27]
+    frequency = 1000
+    alefbot = AlefBot(motor_driver_pins, frequency, ssid="Orange-7C8B", password="YG91AHL2GLJ", repo_url = "https://raw.githubusercontent.com/Seif-El-Dein/tesee/main/", filename= "test1.py")
+    
+    alefbot.set_Speed(30)
+    server_status = True
+    counter = 0
+    lock = _thread.allocate_lock()
+    try:
+        while True:
+            alefbot.key = "None"
+            alefbot.reset_command()
+            print(alefbot.command_handler.wifi.isconnected())
+            if alefbot.command_handler.wifi.isconnected():
+                if server_status == False:
+                    alefbot.command_handler.reconnect_to_broker()
+                    server_status = True
+                Internal_LED.ON()
+                command = alefbot.waiting_commands()
+                if command is None:
+                    command = alefbot.reset_command()
+            elif not alefbot.command_handler.wifi.isconnected():
+                Internal_LED.ON(delay=0.02)
+                Internal_LED.OFF(delay=0.02)
+                server_status = False 
+            alefbot.readCard()
+            print(command)
+            
+            if command == "FIRMWARE":
+                alefbot.command_handler.download_and_install_update_if_available()
+                
+            if command == "say about":
+                alefbot.play_voiceTrack(language= "Arabic", catagory= "Sentence", track_name="About me")
+                sleep(18)
+            
+            if ',' in command:
+                print(command)
+                scenario_list = command.split(',')
+                print(scenario_list)
+                for block in scenario_list:
+                    if block == "Forward":
+                        alefbot.move_Forward(moving_delay_ms=1000)
+                        alefbot.blink_eyes()
+                        print(block)
+
+                    elif block == "Backward":
+                        alefbot.move_Backward(moving_delay_ms=1000)
+                        alefbot.blink_eyes()
+                        print(block)
+                
+                    elif block == "Right":
+                        alefbot.look_right()
+                        alefbot.turnRight(moving_delay_ms=1000)
+                        alefbot.blink_eyes()
+                        print(block)
+                
+                    elif block == "Left":
+                        alefbot.look_left()
+                        alefbot.turnLeft(moving_delay_ms=1000)
+                        alefbot.blink_eyes()
+                        print(block)
+                    alefbot.Stop(stop_delay_ms=1000)
+                 
+            if '=' in command:
+                equation = command[0:3]
+                result = command[4:]
+                
+                if eval(equation) == int(result):
+                    alefbot.play_voiceTrack(language= "English", catagory= "Sentence", track_name="Right Answer")
+                    alefbot.trophy()
+                else:
+                    alefbot.play_voiceTrack(language= "English", catagory= "Sentence", track_name="Try again")
+                print(equation)
+
+            if command == "Forward":
+                alefbot.move_Forward(moving_delay_ms=200)
+                alefbot.blink_eyes()
+                print(command)
+
+            elif command == "Backward":
+                alefbot.move_Backward(moving_delay_ms=200)
+                alefbot.blink_eyes()
+                print(command)
+                
+            elif command == "Right":
+                alefbot.look_right()
+                alefbot.turnRight(moving_delay_ms=200)
+                alefbot.blink_eyes()
+                print(command)
+                
+            elif command == "Left":
+                alefbot.look_left()
+                alefbot.turnLeft(moving_delay_ms=200)
+                alefbot.blink_eyes()
+                print(command)
+            
+            elif alefbot.key == English_words.get("Cat") or command == "CAT":
+                alefbot.play_voiceTrack(language= "English", catagory= "Word", track_name="Cat")
+                sleep_ms(1000)
+                
+            elif alefbot.key == Arabic_letters.get("Alef"):
+                alefbot.play_voiceTrack(language= "Arabic", catagory= "Letter", track_name="Alef")
+                sleep_ms(1000)
+            
+            else:
+                alefbot.Stop()
+                
+            if alefbot.key == WiFi_reconnect.get("WiFi"):
+                alefbot.reconnect_WiFi()
+                alefbot.blink_eyes()
+                    
+            counter +=1
+            if counter == 50:
+                counter = 0
+                lock.acquire()
+                thread = _thread.start_new_thread(alefbot.blink_eyes, ())
+                lock.release()
+                
+
+            
+    except KeyboardInterrupt:
+        print("Exit test phase")
